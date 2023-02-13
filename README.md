73636e3b7fbee771743ca260f2b81ecf1c414917
takuya <55338+takuya@users.noreply.github.com>
2023-02-14 00:51:10 +0900

    initial

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# openwrt-uci-dhcp-sort
-sort openwrt dhcp hosts
+# openwrt-uci-dhcp-sort ipv4
+
+Sort openwrt dhcp hosts ipv4
+
+No v6 available.
+if you uci have a v6 static lease dhcp , this script will disrupt uci.
+
+# how to use 
+
+Copy file into OpenWrt.  Run by Lua
+
+```
+lua uci-sort-dhcp-hosts.lua
+```
+
+
+# what does this script do.
+
+- Backup dhcp.@host to /tmp
+- uci show dhcp and eval dhcp.@host to lua
+- sort dhcp.hosts by ip addr 
+- remove exists by `uci del dhcp.host..`
+- add sorted by `uci add dhcp.host `
+- and commit `uci commit dhcp`
+
+# how to sort ip address 
+
+sort interger by castin ipv4 to Int.
+
+# uci dhcp sample 
+
+
+```sh
+
+### get current dhcp hosts
+uci show dhcp
+
+### revert changes 
+uci revert dhcp 
+
+### commit dhcp
+uci commit dhcp 
+
+### remove all exist dhcp.hosts
+while uci del dhcp.@host[-1] ; do : ;  done;
+
+### add a new dhcp.host
+uci add dhcp host
+uci set dhcp.@host[-1].dns='1'
+uci set dhcp.@host[-1].mac='dc:a6:32:dd:23:c6'
+uci set dhcp.@host[-1].ip='192.168.1.22'
+uci set dhcp.@host[-1].leasetime='86400'
+uci set dhcp.@host[-1].name='raspi-ubuntu'
+
+```
+
+# uci show dhcp via ssh 
+
+if you are using ruby or python or JavaScript(node), you can rewrie via ssh.
+
+### sample
+```
+ssh root@192.168.1.1 'uci show dhcp' \
+  | \grep dhcp.@host | sed -E 's/@//' | sed -E 's/=host/={}/' >> dhcp.rb
+
+```
---
+++ uci-sort-dhcp-hosts.lua
@@ -0,0 +1,132 @@
+-- local inspect = require 'inspect'
+
+function get_uci_dhcp()
+  -- local cmd = 'ssh root@192.168.1.1 "uci show dhcp" '
+  local cmd = 'uci show dhcp'
+  local handle = io.popen(cmd)
+  local result = handle:read("*a")
+  handle:close()
+
+  return result
+
+end
+function backup_uci_dhcp_host()
+  list = get_uci_dhcp()
+  --
+  f_name = '/tmp/dhcp-back.'..os.date('%Y-%m-%d.%s')
+  out = io.open(f_name,"w")
+  out:write(list.."\n")
+  out:close()
+  --
+  print(string.format("dump dhcp.host to %s",f_name))
+
+  return true
+
+end
+
+function uci_dhcp_host_to_lua_array(cmd_result)
+  cmd_result = string.gsub(cmd_result,'@','')
+  cmd_result = string.gsub(cmd_result,']=host',']={}')
+  --
+  lines = {}
+  for line in cmd_result:gmatch("[^\r\n]+") do
+    if string.match(line,'dhcp\.host') then
+      table.insert(lines,line)
+    end
+  end
+  str_array = ""
+  for i,e in pairs(lines) do
+    str_array = str_array..e.."\n"
+  end
+  str_array = str_array.."table.insert(dhcp.host, dhcp.host[0])\n"
+  str_array = str_array.."dhcp.host[0]  = nil\n"
+  -- print(str_array)
+  dhcp={}
+  dhcp.host={}
+  -- eval uci
+  local add_array = loadstring(str_array)
+  add_array()
+
+  return dhcp
+end
+
+function ip4_to_int(ip)
+  t = {}
+  for chunk in string.gmatch( ip , "([0-9]+)") do
+    table.insert(t,chunk)
+  end
+  ip_num = 0
+  for i,e in pairs(t) do
+    ip_num = ip_num  + e*256^(4-i)
+  end
+
+  return ip_num
+
+end
+
+function sort_by_ip_addr(dhcp)
+  for i,e in pairs(dhcp.host) do
+    e.ip_num = ip4_to_int(e.ip)
+  end
+  table.sort(dhcp.host, function(a,b) return a.ip_num < b.ip_num end)
+
+  return dhcp
+
+end
+
+function gen_uci_dhcp_add_cmd(dhcp)
+  cmd = ''
+  for i,e in pairs(dhcp.host) do
+    idx = i-1
+    cmd  = cmd..string.format("# No.%03d \n", i)
+    cmd  = cmd.."uci add dhcp host\n"
+    cmd  = cmd..string.format("uci set dhcp.@host[-1].dns='%s'\n", e.dns  )
+    cmd  = cmd..string.format("uci set dhcp.@host[-1].mac='%s'\n", e.mac )
+    cmd  = cmd..string.format("uci set dhcp.@host[-1].ip='%s'\n",  e.ip  )
+    if  e.leasetime then
+      cmd  = cmd..string.format("uci set dhcp.@host[-1].leasetime='%s'\n", e.leasetime  )
+    end
+    cmd  = cmd..string.format("uci set dhcp.@host[-1].name='%s'\n", e.name )
+  end
+
+  return cmd
+
+end
+
+function gen_uci_dhcp_host_delete_cmd()
+  del_cmd = 'while uci del dhcp.@host[-1] 2>/dev/null 1> /dev/null; do : ;  done;'
+  return "# delete exists\n"..del_cmd.."\n# Adding dhcp \n"
+end
+
+
+function gen_uci_sorted_dhcp_host()
+  -- get exists dhcp.host
+  local str  = get_uci_dhcp()
+  local arr = uci_dhcp_host_to_lua_array(str)
+  -- sort dhcp.host
+  arr = sort_by_ip_addr(arr)
+  -- generate command string
+  local cmd = ''
+  cmd = cmd..gen_uci_dhcp_host_delete_cmd(arr)
+  cmd = cmd..gen_uci_dhcp_add_cmd(arr)
+  return cmd
+end
+
+function update_and_commit_uci_with_sorted_dhcp()
+  local commit_cmd = ''
+  commit_cmd = 'uci commit dhcp\n'
+  local cmd = gen_uci_sorted_dhcp_host()
+  cmd = cmd..commit_cmd
+  local handle = io.popen(cmd)
+  local result = handle:read("*a")
+end
+
+
+function main()
+  backup_uci_dhcp_host()
+  update_and_commit_uci_with_sorted_dhcp()
+end
+
+if not pcall(debug.getlocal, 4, 1) then
+  main();
+end
